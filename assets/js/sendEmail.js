8aa001f5496c32c5e758ae9b98e6c50af5a5d2ef
biyelaayanda3 <biyelaayanda3@gmail.com>
2023-03-10 04:45:52 +0200

    index

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,7 +1,7 @@
 function sendemail(){
     var params ={
         name: document.getElementById("name").value,
-        subject: 'Contact Us email',
+        subject: document.getElementById("subject").value,
         email:document.getElementById("email").value,
         message: document.getElementById("message").value
     };
@@ -12,6 +12,7 @@ const templateId ="template_5ix7nl5";
 
 emailjs.send(serviceID,templateId,params).then(res =>{
     document.getElementById("name").value ="";
+    document.getElementById("subject").value="";
     document.getElementById("email").value ="";
     document.getElementById("message").value ="";
     console.log(res);
