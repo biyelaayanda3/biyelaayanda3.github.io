160c2df99db8aa4b69a58273a36e10b49a47c4b3
biyelaayanda3 <biyelaayanda3@gmail.com>
2023-03-10 03:56:31 +0200

    Update sendEmail.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -10,12 +10,7 @@ function sendemail(){
 const serviceID = "service_oh3c5zz";
 const templateId ="template_5ix7nl5";
 
-emailjs.send(serviceID, templateId, {
-    name: document.getElementById("name").value,
-    subject: "Contact Us email",
-    email:document.getElementById("email").value,
-    message: document.getElementById("message").value
-}).then(res =>{
+emailjs.send(serviceID, templateId, params).then(res =>{
     document.getElementById("name").value ="";
     document.getElementById("email").value ="";
     document.getElementById("message").value ="";
