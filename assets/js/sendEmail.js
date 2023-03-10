acade4215b3128759822994ba4c6b5eecdafc701
biyelaayanda3 <biyelaayanda3@gmail.com>
2023-03-10 03:43:22 +0200

    emails

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,21 @@
+function sendemail(){
+    var params ={
+        name: document.getElementById("name").value,
+        subject: "Contact Us email",
+        email:document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+
+
+const serviceID = "service_oh3c5zz";
+const templateId ="template_5ix7nl5";
+
+emailjs.send(serviceID, templateId).then(res =>{
+    document.getElementById("name").value ="";
+    document.getElementById("email").value ="";
+    document.getElementById("message").value ="";
+    console.log(res);
+    alert("message sent");
+}).catch((error)=>
+    console.log(error));
+}
